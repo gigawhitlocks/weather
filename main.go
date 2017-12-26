2a5c2973b4a2a91285e2bd74a9943c4df5fbc1b3
Ian Whitlock <ian@theknown.net>
2017-12-25 23:17:46 -0600

    precipitation maps; help text

--- main.go
+++ main.go
@@ -109,7 +109,13 @@ func main() {
 			query := strings.TrimSpace(strings.TrimPrefix(q, "satellite"))
 			var err error
 			var result *image.NRGBA
-			if result, err = openweathermap.GetSatellite(query); err != nil {
+			var loc *openweathermap.Location
+			if loc, err = openweathermap.GetTileNumbers(query); err != nil {
+				fmt.Fprintf(w, "Bad result from OpenWeatherMap API: %s", err)
+				return
+			}
+
+			if result, err = openweathermap.GetSatellite(loc); err != nil {
 				fmt.Fprintf(w, "Bad result from OpenWeatherMap API: %s", err)
 				return
 			}
@@ -129,9 +135,32 @@ func main() {
 			fmt.Fprintf(w, "https://shouting.online/%s\n", path)
 
 			return
+		case strings.HasPrefix(q, "precip"):
+			query := strings.TrimSpace(strings.TrimPrefix(q, "precip"))
+			var err error
+			var result *image.NRGBA
+			if result, err = openweathermap.GetComposite(query); err != nil {
+				fmt.Fprintf(w, "Bad result from OpenWeatherMap API: %s", err)
+				return
+			}
 
+			uid := fmt.Sprintf("%s%d", query, time.Now().Nanosecond())
+			imagestore.Store(fmt.Sprintf("composite%s.png", uid), result)
+
+			path := fmt.Sprintf("?zip=composite%s.png", uid)
+
+			// links to click
+			if os.Getenv("DEBUG") == "1" {
+				fmt.Fprintf(w, "http://127.0.0.1:8111/%s\n", path)
+				return
+			}
+
+			path = fmt.Sprintf("weather%s", path)
+			fmt.Fprintf(w, "https://shouting.online/%s\n", path)
+
+			return
 		default:
-			fmt.Fprintf(w, "%s", "Invalid command")
+			fmt.Fprintf(w, "%s", "*Commands:*\n\n`!nws`: get a weather report from the NWS's public data source. Use a zip code, expect results to be from airports.\n`!weather`: get the current weather and today's forcast. Use `zip` or `city, state` e.g. `!weather 78703` or `!weather san francisco, ca`\n`!forecast: short-term forecast by zip or city, state`\n`!precip` get a precipitation map of the region centered on provided zip\n`!satellite` get a recent (as old as a week) satellite of a region centered on a zip")
 
 		}
 	})
--- openweathermap/owm.go
+++ openweathermap/owm.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"image/draw"
 	"image/png"
 	"math"
 	"net/http"
@@ -16,6 +17,8 @@ import (
 
 var CityStatePattern, _ = regexp.Compile("[A-Z a-z]+(,?[ \t]+[A-Za-z]+)?")
 var ZipPattern, _ = regexp.Compile("[0-9]{5}")
+
+const zoom = 7
 
 type PrecipitationMap struct {
 }
@@ -38,32 +41,36 @@ func tileNumbers(lat, long float64, zoom int) (int, int) {
 
 var APIKEY = os.Getenv("OWM_API_KEY")
 
-func GetSatellite(query string) (*image.NRGBA, error) {
-	var err error
-	var url string
+type Map int
 
-	switch {
-	case ZipPattern.MatchString(query):
-		url = fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=%s",
-			query, APIKEY)
-	default:
-		return nil, fmt.Errorf("ah shit")
+const (
+	_        = iota
+	Base Map = 1 + iota
+	Clouds
+	Precipitation
+)
 
+func GetTiles(xtile, ytile int, mt Map) (*image.NRGBA, error) {
+	var baseurl string
+	var querystring string
+
+	if mt == Base {
+		baseurl = "https://sat.owm.io/sql"
+		// querystring = fmt.Sprintf("?APPID=%s&op=rgb&from=l8&select=b4,b3,b2&order=best", APIKEY)
+		querystring = fmt.Sprintf("?APPID=%s&op=rgb&from=cloudless&select=red,green,blue&order=best", APIKEY)
+	} else if mt == Clouds || mt == Precipitation {
+		baseurl = "https://tile.openweathermap.org/map"
+		var layer string
+		if mt == Clouds {
+			layer = "/clouds_new"
+		} else {
+			layer = "/precipitation_new"
+		}
+		querystring = fmt.Sprintf(".png?cities=true&appid=%s", APIKEY)
+		baseurl = fmt.Sprintf("%s%s", baseurl, layer)
+	} else {
+		return nil, fmt.Errorf("Unrecognized map type requested")
 	}
-
-	var resp *http.Response
-	if resp, err = http.Get(url); err != nil {
-		return nil, err
-	}
-
-	c := new(Location)
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(c); err != nil {
-		return nil, err
-	}
-
-	zoom := 12
-	xtile, ytile := tileNumbers(c.Lat, c.Long, zoom)
 
 	// get the images
 	type imagePos struct {
@@ -75,10 +82,18 @@ func GetSatellite(query string) (*image.NRGBA, error) {
 	imagesChannel := make(chan interface{}, 9)
 	for x := 0; x < 3; x++ {
 		for y := 0; y < 3; y++ {
-			go func(x, y int) {
-				url := fmt.Sprintf(
-					"https://sat.owm.io/sql/%d/%d/%d?APPID=%s&op=rgb&from=l8t&select=b4,b3,b2&order=best",
-					zoom, xtile+(x-1), ytile+(y-1), APIKEY)
+			url := fmt.Sprintf(
+				"%s/%d/%d/%d%s",
+				baseurl,
+				zoom,
+				xtile+(x-1),
+				ytile+(y-1),
+				querystring)
+			fmt.Printf("%s\n", url)
+
+			go func(x, y int, url string) {
+				var resp *http.Response
+				var err error
 				if resp, err = http.Get(url); err != nil {
 					imagesChannel <- err
 					return
@@ -95,7 +110,7 @@ func GetSatellite(query string) (*image.NRGBA, error) {
 					X:     x,
 					Y:     y,
 				}
-			}(x, y)
+			}(x, y, url)
 		}
 	}
 
@@ -118,6 +133,109 @@ func GetSatellite(query string) (*image.NRGBA, error) {
 		}
 	}
 
+	return stitchTiles(images), nil
+}
+
+func GetTileNumbers(query string) (*Location, error) {
+	var err error
+	var url string
+
+	switch {
+	case ZipPattern.MatchString(query):
+		url = fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=%s",
+			query, APIKEY)
+	default:
+		return nil, fmt.Errorf("Satellite endpoint only accepts zip codes :(")
+	}
+
+	var resp *http.Response
+	if resp, err = http.Get(url); err != nil {
+		return nil, err
+	}
+
+	c := new(Location)
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(c); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func GetSatellite(c *Location) (*image.NRGBA, error) {
+	xtile, ytile := tileNumbers(c.Lat, c.Long, zoom)
+	return GetTiles(xtile, ytile, Base)
+}
+
+func GetComposite(query string) (*image.NRGBA, error) {
+	l, err := GetTileNumbers(query)
+	if err != nil {
+		return nil, err
+	}
+	type MapResult struct {
+		self    *image.NRGBA
+		mapType Map
+	}
+	xtile, ytile := tileNumbers(l.Lat, l.Long, zoom)
+	results := make(chan interface{}, 3)
+
+	go func() {
+		var basemap *image.NRGBA
+		if basemap, err = GetSatellite(l); err != nil {
+			results <- err
+		}
+		results <- &MapResult{
+			self:    basemap,
+			mapType: Base,
+		}
+	}()
+
+	// go func() {
+	// 	var basemap *image.NRGBA
+	// 	if basemap, err = GetTiles(xtile, ytile, Clouds); err != nil {
+	// 		results <- err
+	// 	}
+	// 	results <- &MapResult{
+	// 		self:    basemap,
+	// 		mapType: Clouds,
+	// 	}
+	// }()
+
+	go func() {
+		var basemap *image.NRGBA
+		if basemap, err = GetTiles(xtile, ytile, Precipitation); err != nil {
+			results <- err
+		}
+		results <- &MapResult{
+			self:    basemap,
+			mapType: Precipitation,
+		}
+
+	}()
+
+	im := make(map[Map]*image.NRGBA)
+	for x := 0; x < 2; x++ {
+		r := <-results
+		switch r := r.(type) {
+		case *MapResult:
+			im[r.mapType] = r.self
+		case error:
+			return nil, r
+		}
+	}
+
+	result := im[Base]
+	// result = imaging.Overlay(result, im[Precipitation], image.Pt(0, 0), .5)
+	m := image.NewRGBA(image.Rect(0, 0, 512*3, 512*3))
+	green := color.RGBA{0, 255, 0, 255}
+	draw.Draw(m, m.Bounds(), &image.Uniform{green}, image.ZP, draw.Src)
+	draw.DrawMask(m, m.Bounds(), m, image.Pt(0, 0), im[Precipitation], image.Pt(0, 0), draw.Src)
+
+	result = imaging.Overlay(result, m, image.Pt(0, 0), .9)
+	return result, nil
+}
+
+func stitchTiles(images [3][3]*image.Image) *image.NRGBA {
 	// stitch together the tiles
 	dst := imaging.New(768, 768, color.NRGBA{0, 0, 0, 0})
 	for x := 0; x < len(images); x++ {
@@ -125,5 +243,5 @@ func GetSatellite(query string) (*image.NRGBA, error) {
 			dst = imaging.Paste(dst, *images[x][y], image.Pt(256*x, 256*y))
 		}
 	}
-	return dst, nil
+	return dst
 }
