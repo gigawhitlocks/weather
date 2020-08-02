b8bf36c6efd4889c06ab5f03c9b57a30c2b6cf46
Ian Whitlock <ian@theknown.net>
2020-08-01 20:52:01 -0500

    Add geocoding support with an upstream API

---
+++ geocoding/geocoder.go
@@ -0,0 +1,217 @@
+package geocoding
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+var ApiKey = os.Getenv("GEOCODING_KEY")
+var ApiURL = fmt.Sprintf("https://api.opencagedata.com/geocode/v1/json?key=%s", ApiKey)
+
+type Geocoder interface {
+	Geocode(location string) (lat, long float64)
+}
+
+type OpenCageDataGeocodeResponse struct {
+	Documentation string `json:"documentation"`
+	Licenses      []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"licenses"`
+	Rate struct {
+		Limit     int `json:"limit"`
+		Remaining int `json:"remaining"`
+		Reset     int `json:"reset"`
+	} `json:"rate"`
+	Results []struct {
+		Annotations struct {
+			DMS struct {
+				Lat string `json:"lat"`
+				Lng string `json:"lng"`
+			} `json:"DMS"`
+			FIPS struct {
+				County string `json:"county"`
+				State  string `json:"state"`
+			} `json:"FIPS"`
+			MGRS       string `json:"MGRS"`
+			Maidenhead string `json:"Maidenhead"`
+			Mercator   struct {
+				X float64 `json:"x"`
+				Y float64 `json:"y"`
+			} `json:"Mercator"`
+			OSM struct {
+				EditURL string `json:"edit_url"`
+				NoteURL string `json:"note_url"`
+				URL     string `json:"url"`
+			} `json:"OSM"`
+			UNM49 struct {
+				Regions struct {
+					AMERICAS        string `json:"AMERICAS"`
+					NORTHERNAMERICA string `json:"NORTHERN_AMERICA"`
+					US              string `json:"US"`
+					WORLD           string `json:"WORLD"`
+				} `json:"regions"`
+				StatisticalGroupings []string `json:"statistical_groupings"`
+			} `json:"UN_M49"`
+			Callingcode int `json:"callingcode"`
+			Currency    struct {
+				AlternateSymbols     []string `json:"alternate_symbols"`
+				DecimalMark          string   `json:"decimal_mark"`
+				DisambiguateSymbol   string   `json:"disambiguate_symbol"`
+				HTMLEntity           string   `json:"html_entity"`
+				IsoCode              string   `json:"iso_code"`
+				IsoNumeric           string   `json:"iso_numeric"`
+				Name                 string   `json:"name"`
+				SmallestDenomination int      `json:"smallest_denomination"`
+				Subunit              string   `json:"subunit"`
+				SubunitToUnit        int      `json:"subunit_to_unit"`
+				Symbol               string   `json:"symbol"`
+				SymbolFirst          int      `json:"symbol_first"`
+				ThousandsSeparator   string   `json:"thousands_separator"`
+			} `json:"currency"`
+			Flag     string  `json:"flag"`
+			Geohash  string  `json:"geohash"`
+			Qibla    float64 `json:"qibla"`
+			Roadinfo struct {
+				DriveOn string `json:"drive_on"`
+				SpeedIn string `json:"speed_in"`
+			} `json:"roadinfo"`
+			Sun struct {
+				Rise struct {
+					Apparent     int `json:"apparent"`
+					Astronomical int `json:"astronomical"`
+					Civil        int `json:"civil"`
+					Nautical     int `json:"nautical"`
+				} `json:"rise"`
+				Set struct {
+					Apparent     int `json:"apparent"`
+					Astronomical int `json:"astronomical"`
+					Civil        int `json:"civil"`
+					Nautical     int `json:"nautical"`
+				} `json:"set"`
+			} `json:"sun"`
+			Timezone struct {
+				Name         string `json:"name"`
+				NowInDst     int    `json:"now_in_dst"`
+				OffsetSec    int    `json:"offset_sec"`
+				OffsetString string `json:"offset_string"`
+				ShortName    string `json:"short_name"`
+			} `json:"timezone"`
+			What3Words struct {
+				Words string `json:"words"`
+			} `json:"what3words"`
+			Wikidata string `json:"wikidata"`
+		} `json:"annotations"`
+		Bounds struct {
+			Northeast struct {
+				Lat float64 `json:"lat"`
+				Lng float64 `json:"lng"`
+			} `json:"northeast"`
+			Southwest struct {
+				Lat float64 `json:"lat"`
+				Lng float64 `json:"lng"`
+			} `json:"southwest"`
+		} `json:"bounds"`
+		Components struct {
+			ISO31661Alpha2 string `json:"ISO_3166-1_alpha-2"`
+			ISO31661Alpha3 string `json:"ISO_3166-1_alpha-3"`
+			Category       string `json:"_category"`
+			Type           string `json:"_type"`
+			City           string `json:"city"`
+			Continent      string `json:"continent"`
+			Country        string `json:"country"`
+			CountryCode    string `json:"country_code"`
+			County         string `json:"county"`
+			State          string `json:"state"`
+			StateCode      string `json:"state_code"`
+		} `json:"components"`
+		Confidence int    `json:"confidence"`
+		Formatted  string `json:"formatted"`
+		Geometry   struct {
+			Lat float64 `json:"lat"`
+			Lng float64 `json:"lng"`
+		} `json:"geometry"`
+	} `json:"results"`
+	Status struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	} `json:"status"`
+	StayInformed struct {
+		Blog    string `json:"blog"`
+		Twitter string `json:"twitter"`
+	} `json:"stay_informed"`
+	Thanks    string `json:"thanks"`
+	Timestamp struct {
+		CreatedHTTP string `json:"created_http"`
+		CreatedUnix int    `json:"created_unix"`
+	} `json:"timestamp"`
+	TotalResults int `json:"total_results"`
+}
+
+type OpenCageData struct {
+}
+
+func buildQuery(query string) string {
+	return fmt.Sprintf("%s&q=%s", ApiURL, url.QueryEscape(query))
+}
+
+func doGet(url string) (ocdgr *OpenCageDataGeocodeResponse, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to fetch geocode data from Open Cage Data")
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read response body")
+	}
+	ocdgr = new(OpenCageDataGeocodeResponse)
+	err = json.Unmarshal(body, ocdgr)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal JSON from response body")
+	}
+	return
+}
+
+type Coordinates struct {
+	Latitude  float64
+	Longitude float64
+}
+
+func doGeocode(location string) (*OpenCageDataGeocodeResponse, error) {
+	response, err := doGet(buildQuery(location))
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to fetch coordinates for location %s", location)
+	}
+
+	if len(response.Results) == 0 {
+		return nil, errors.Errorf("no results found for location '%s'", location)
+	}
+
+	return response, nil
+}
+
+func Geocode(location string) (*Coordinates, error) {
+	response, err := doGeocode(location)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to fetch lat/long for location '%s'", location)
+	}
+	return &Coordinates{
+		Latitude:  response.Results[0].Geometry.Lat,
+		Longitude: response.Results[0].Geometry.Lng,
+	}, nil
+}
+
+func Map(location string) (string, error) {
+	response, err := doGeocode(location)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to fetch map for location '%s'", location)
+	}
+
+	return response.Results[0].Annotations.OSM.URL, nil
+}
---
+++ geocoding/geocoder_test.go
@@ -0,0 +1,36 @@
+package geocoding
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain(m *testing.M) {
+	setup()
+	ret := m.Run()
+	if ret == 0 {
+		teardown()
+	}
+	os.Exit(ret)
+}
+
+func setup() {
+	if "" == os.Getenv("GEOCODING_KEY") {
+		panic("must set GEOCODING_KEY to run tests")
+	}
+}
+
+func teardown() {
+
+}
+
+func TestGeocode(t *testing.T) {
+	c, err := Geocode("austin")
+	require.NoError(t, err)
+	require.NotNil(t, c)
+	assert.Equal(t, 30.2711286, c.Latitude)
+	assert.Equal(t, -97.7436995, c.Longitude)
+}
